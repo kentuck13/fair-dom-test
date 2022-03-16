39b5dc15b63bcba7c756e960bde1795b1f0d7e47
Alexander <kentuck465@gmail.com>
2022-03-16 17:27:16 +0500

    Add shell script for creating tables

--- run_tests.sh
+++
@@ -1,3 +0,0 @@
-docker build -t test .
-docker-compose -f docker-compose.yml run web
-docker-compose down
--- tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from dynamodb import dynamodb
-import create_tables
-
-
-class TestDynamodb(unittest.TestCase):
-    def test_create_tables(self):
-        table = dynamodb.Table('users')
-        self.assertEqual(table.item_count, 0)
